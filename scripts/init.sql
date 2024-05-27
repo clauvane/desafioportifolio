72446ea08b905bb40fe415856c69bc24d1316cfd
clauvane <clauvane1708@gmail.com>
2024-05-27 17:48:30 -0300

    fix: corrigindo constraints e removendo import desnecessario.

--- scripts/init.sql
+++ scripts/init.sql
@@ -37,11 +37,11 @@ CREATE TABLE IF NOT EXISTS projeto (
 CREATE TABLE IF NOT EXISTS membros (
     idprojeto bigint NOT NULL,
     idpessoa bigint NOT NULL,
-    CONSTRAINT pk_membros_projeto PRIMARY KEY (idprojeto),
+    CONSTRAINT pk_membros PRIMARY KEY (idprojeto, idpessoa),
     CONSTRAINT fk_membros_pessoa FOREIGN KEY (idpessoa)
-        REFERENCES projeto (id) MATCH SIMPLE
-        ON UPDATE NO ACTION ON DELETE NO ACTION,
-    CONSTRAINT fk_pessoa FOREIGN KEY (idpessoa)
         REFERENCES pessoa (id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CONSTRAINT fk_membros_projeto FOREIGN KEY (idprojeto)
+        REFERENCES projeto (id) MATCH SIMPLE
         ON UPDATE NO ACTION ON DELETE NO ACTION
 );
